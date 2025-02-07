6880137f0375b406d1b479df2d849161be5c415e
Bojan SKI <codeucenje@gmail.com>
2025-02-07 16:52:57 +0100

    utils folder in root create. helpers.php with initial helper funcs created. tailwind & daisyui cdn added

---
+++ App/views/home.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- tailwindcss - cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" referrerpolicy="no-referrer" />
+
+    <!-- daisyui - cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/daisyui/4.12.23/full.css" integrity="sha512-liVdpj1E7WACFJBjb9MexsvLC33vmuHcrmR+nw42OaRl13zpHWHJofEKTP3M84Ah82zJTBvei6ggHlSv9aLz8Q==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="../../public/css/style.css">
+
+    <title>Daily Planer</title>
+</head>
+
+<body>
+    <div class="bg-blue-500 text-white p-4">
+        Hello, Tailwind in PHP!
+    </div>
+</body>
+
+</html>
---
+++ utils/helpers.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+
+// LOAD PATH
+function basePath(string $path = ''): string
+{
+    return __DIR__ . '/' . $path;
+}
+
+// LOAD VIEW
+function loadView(string $name): void
+{
+    (string) $viewPath = basePath("App/views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$name}' not found!";
+    }
+}
+
+// LOAD PARTIAL
+function loadPartial(string $name): void
+{
+    (string) $partialPath = basePath("App/views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name}' not found!";
+    }
+}
+
+// REDIRECT USER
+function redirectUser(string $url): void
+{
+    header("Location: {$url}");
+    exit();
+}
+
+// INSPECT DATA
+function inspect(mixed $value): void
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+}
+
+// INSPECT DATA AND END PROGRAM
+function inspectAndDie(mixed $value): void
+{
+    echo "<pre>";
+    die(var_dump($value));
+    echo "</pre>";
+}
