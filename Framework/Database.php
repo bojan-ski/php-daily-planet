1dad89d7825d258674e521a025f72e8e096e1c1c
Bojan SKI <codeucenje@gmail.com>
2025-02-09 09:37:53 +0100

    Database class in Framework folder created

---
+++ Framework/Database.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework;
+
+use PDO;
+use PDOException;
+use Exception;
+
+class Database
+{
+    public $conn;
+
+    public function __construct(string $config)
+    {
+        (string) $dns = "mysql:host={$config['host']};dbname={$config['dbname']}";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+
+        try {
+            $this->conn = new PDO($dns, $config['username'], $config['password'], $options);
+        } catch (PDOException $e) {
+            throw new Exception("Database connection failed: {$e->getMessage()}");
+        }
+    }
+
+    public function dbQuery(string $query, array $params = []): mixed
+    {
+        try {
+            $str = $this->conn->prepare($query);
+
+            foreach ($params as $param => $value) {
+                $str->bindValue(':' . $param, $value);
+            }
+
+            $str->execute();
+
+            return $str;
+        } catch (PDOException $e) {
+            throw new Exception("Query failed to execute: {$e->getMessage()}");
+        }
+    }
+}
