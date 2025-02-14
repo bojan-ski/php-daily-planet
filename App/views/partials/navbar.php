ccb7f58fb6d4153fa17f32805243ce3920e56ab8
Bojan SKI <codeucenje@gmail.com>
2025-02-14 10:45:45 +0100

    navbar updated. mobile responsive

--- App/views/partials/navbar.php
+++ App/views/partials/navbar.php
@@ -5,17 +5,55 @@ use Framework\Session;
 ?>
 
 <nav class="bg-gray-100 text-center py-2">
-    <a href="/" class="nav-link btn btn-ghost text-7xl mx-7">
-        Home
-    </a>
-    <a href="/articles" class="nav-link btn btn-ghost text-xl mx-7">
-        Articles
-    </a>
-    <?php if (Session::exist('user') && Session::get('user')['role'] == 'reader'): ?>
-        <?php loadPartial('navLinks/reader'); ?>
-    <?php elseif (Session::exist('user') && Session::get('user')['role'] == 'author'): ?>
-        <?php loadPartial('navLinks/author'); ?>
-    <?php elseif (Session::exist('user') && Session::get('user')['role'] == 'admin'): ?>
-        <?php loadPartial('navLinks/admin'); ?>
-    <?php endif; ?>
+    <div class="block lg:hidden">
+        <div class="dropdown">
+            <div tabindex="0" role="button" class="btn btn-ghost">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h8m-8 6h16" />
+                </svg>
+            </div>
+            <div
+                tabindex="0"
+                class="dropdown-content bg-base-100 rounded-box z-[10] w-72 p-2 shadow">
+                <a href="/" class="nav-link btn btn-ghost text-7xl mx-7">
+                    Home
+                </a>
+                <a href="/articles" class="nav-link btn btn-ghost text-xl mx-7">
+                    Articles
+                </a>
+                <?php if (Session::exist('user') && Session::get('user')['role'] == 'reader'): ?>
+                    <?php loadPartial('navLinks/reader'); ?>
+                <?php elseif (Session::exist('user') && Session::get('user')['role'] == 'author'): ?>
+                    <?php loadPartial('navLinks/author'); ?>
+                <?php elseif (Session::exist('user') && Session::get('user')['role'] == 'admin'): ?>
+                    <?php loadPartial('navLinks/admin'); ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+
+    <div class="hidden lg:block">
+        <a href="/" class="nav-link btn btn-ghost text-7xl mx-7">
+            Home
+        </a>
+        <a href="/articles" class="nav-link btn btn-ghost text-xl mx-7">
+            Articles
+        </a>
+        <?php if (Session::exist('user') && Session::get('user')['role'] == 'reader'): ?>
+            <?php loadPartial('navLinks/reader'); ?>
+        <?php elseif (Session::exist('user') && Session::get('user')['role'] == 'author'): ?>
+            <?php loadPartial('navLinks/author'); ?>
+        <?php elseif (Session::exist('user') && Session::get('user')['role'] == 'admin'): ?>
+            <?php loadPartial('navLinks/admin'); ?>
+        <?php endif; ?>
+    </div>
 </nav>
